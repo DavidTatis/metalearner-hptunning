794521ab1f6d3ec4d014cf31d97b70c28a0d16f0
DavidTatis <davidtatis96@gmail.com>
2022-09-01 20:34:39 +0000

    Writing a good metadataset

--- Metalearner.py
+++ Metalearner.py
@@ -11,7 +11,7 @@ import pathlib
 
 # %%
 
-def write_meta_data(data_file_name,dataset_column_names,x_column_names,metric_name,error_metric=True,add_header=False,dim=1):
+def write_meta_data(data_file_name,dataset_column_names,header_metadata,metric_name,error_metric=True,add_header=False,dim=1,dataset_name="empty"):
     dataset=pd.read_csv(os.path.dirname(os.path.abspath(__file__))+"/"+data_file_name,names=dataset_column_names)
     y=np.zeros(len(dataset))
     
@@ -25,11 +25,12 @@ def write_meta_data(data_file_name,dataset_column_names,x_column_names,metric_na
         acc_ratio=max_acc/dataset[metric_name]
         y=acc_ratio
     
-    x=dataset[x_column_names]
-    x["dimension"]=dim
+    x=dataset[header_metadata]
+    x.loc[:,"dimension"]=dim
+    x.loc[:,"dataset"]=dataset_name
     xy=pd.concat([x,y],axis=1)
-    xy.columns=[*x_column_names,'dimension','y']
-    # if add_header: pd.DataFrame(columns=x_column_names).to_csv(os.path.dirname(os.path.abspath(__file__))+"/"'data/metadataset.csv', mode='a', header=False,index=False)
+    xy.columns=[*header_metadata,"dimension","dataset",'y']
+    # if add_header: pd.DataFrame(columns=header_metadata).to_csv(os.path.dirname(os.path.abspath(__file__))+"/"'data/metadataset.csv', mode='a', header=False,index=False)
 
     xy.to_csv(os.path.dirname(os.path.abspath(__file__))+"/"'data/metadataset.csv', mode='a', header=add_header,index=False)
 
@@ -177,7 +178,7 @@ def meta_learner(n_top_hp_to_select,dataset_column_names,x_column_names,metric_n
     top_lr,top_bz,top_layers,top_af,finish_order=get_top_hp_combination(n_top_hp_to_select,predictions)
     return top_lr,top_bz,top_layers,top_af,finish_order
 
-def test(data_file_name,add_header=False,dim=1):
+def test(data_file_name,add_header=False,dim=1,dataset_name="empty"):
     to_categorical_column_names=["activation_function"]
     
     max_epochs=10
@@ -217,17 +218,25 @@ def test(data_file_name,add_header=False,dim=1):
     to_categorical_column_names=["activation_function","architecture"]
     
 
-    write_meta_data(data_file_name,dataset_column_names,header_metadata,metric_name,error_metric=True,add_header=add_header,dim=dim)
+    write_meta_data(data_file_name,dataset_column_names,header_metadata,metric_name,error_metric=True,add_header=add_header,dim=dim,dataset_name=dataset_name)
     # top_lr,top_bz,top_layers,top_af,finish_order=meta_learner(2,dataset_column_names,x_column_names,metric_name,to_categorical_column_names,data_file_name,
     #                                                             num_features,training_and_validation_samples,n_layers,learning_rate,batch_size,activation_function)
     # print(top_lr,top_bz,top_layers,top_af,finish_order)
     
 
 # %%
-data_file_name="data/1d_fcmnr.csv"
-test(data_file_name,True,1)
-data_file_name="data/1d_fcunet.csv"
-test(data_file_name,dim=1)
-data_file_name="data/1d_irnet.csv"
-test(data_file_name,dim=1)
+
+
+dataset_name="flight-price-prediction"
+test("data/1d_fcunet.csv",dim=1,add_header=True,dataset_name=dataset_name)
+test("data/1d_irnet.csv",dim=1,dataset_name=dataset_name)
+test("data/1d_fcmnr.csv",dim=1,dataset_name=dataset_name)
+
+dataset_name="brain-mri-segmentation"
+test("data/2d_mnr.csv",dim=2,dataset_name=dataset_name)
+test("data/2d_unet.csv",dim=2,dataset_name=dataset_name)
+test("data/2d_irnet.csv",dim=2,dataset_name=dataset_name)
+
+dataset_name="indian-pines"
+test("data/3d_cnn.csv",dim=3,dataset_name=dataset_name)
 
