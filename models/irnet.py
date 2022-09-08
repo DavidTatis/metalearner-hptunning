2197f5b9042d2bf797169c50050b1a2080cce715
DavidTatis <davidtatis96@gmail.com>
2022-09-08 01:27:32 +0000

    irnet for materials

--- models/irnet.py
+++ models/irnet.py
@@ -45,3 +45,45 @@ def irnet_model(input_shape,n_layers,activation_function,learning_rate):
       metrics=['accuracy'])
     
     return model
+
+
+
+def irnet_materials_model(input_shape,n_layers,activation_function,learning_rate):   
+    input_vec = Input(shape=input_shape)
+    x0=Activation(activation_function)(BatchNormalization()(Dense(1024)(input_vec)))
+    m1=concatenate([input_vec, x0], axis=-1)
+    
+    for i in range(n_layers):
+        x1=Activation(activation_function)(BatchNormalization()(Dense(1024)(m1)))
+        m1=concatenate([m1, x1], axis=-1)
+    for i in range(n_layers):
+        x1=Activation(activation_function)(BatchNormalization()(Dense(512)(m1)))
+        m1=concatenate([m1, x1], axis=-1)
+
+    for i in range(n_layers):
+        x1=Activation(activation_function)(BatchNormalization()(Dense(256)(m1)))
+        m1=concatenate([m1, x1], axis=-1)
+
+    for i in range(n_layers):
+        x1=Activation(activation_function)(BatchNormalization()(Dense(128)(m1)))
+        m1=concatenate([m1, x1], axis=-1)
+        
+    for i in range(n_layers):
+        x1=Activation(activation_function)(BatchNormalization()(Dense(64)(m1)))
+        m1=concatenate([m1, x1], axis=-1)
+
+    for i in range(n_layers):
+        x1=Activation(activation_function)(BatchNormalization()(Dense(32)(m1)))
+        m1=concatenate([m1, x1], axis=-1)
+    for i in range(n_layers):
+        x1=Activation(activation_function)(BatchNormalization()(Dense(16)(m1)))
+        m1=concatenate([m1, x1], axis=-1)
+            
+    xf=Dense(1,activation='linear')(m1)
+
+    model=Model(input_vec,xf)
+    model.compile(
+      optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+      loss=tf.keras.losses.mean_absolute_error,
+      metrics=[tf.keras.metrics.MeanAbsoluteError()])
+    return model
